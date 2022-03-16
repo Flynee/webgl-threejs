2366f4291e1f8647e8c55a1a3e38e1b54928a3f3
zhangzhzh <zhangzhzh@uino.com>
2022-03-16 14:11:48 +0800

    feat: 添加描边

--- threejs/ray-outline/index.js
+++ threejs/ray-outline/index.js
@@ -3,55 +3,41 @@ function main() {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer();
+    const group = new THREE.Group();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.querySelector('#div3d').appendChild(renderer.domElement);
+    scene.add(group);
 
     // panel
-    planes = [
-        new THREE.Plane( new THREE.Vector3( - 10, 0, 0 ), 0 ),
-        new THREE.Plane( new THREE.Vector3( 0, - 10, 0 ), 0 ),
-        new THREE.Plane( new THREE.Vector3( 0, 0, - 10 ), 0 )
-    ];
-    planeHelpers = planes.map( p => new THREE.PlaneHelper( p, 10, 0xffffff ) );
-    planeHelpers.forEach( ph => {
+    const planeGemetry = new THREE.PlaneGeometry(12, 12);
+    const planeMaterial = new THREE.MeshLambertMaterial({color: 0x333333, side: THREE.DoubleSide });
+    const planeMesh = new THREE.Mesh(planeGemetry, planeMaterial);
+    planeMesh.rotation.x -= Math.PI * 0.5;
+    planeMesh.position -= 1.5;
+    group.add(planeMesh);
 
-        scene.add( ph );
-
-    } );
-
-    // axes helper
-    const axesHelper = new THREE.AxesHelper(10);
-    const xAxisColor = new THREE.Color('#ff0000');
-    const yAxisColor = new THREE.Color('#00ff00');
-    const zAxisColor = new THREE.Color('#0000ff');
-    axesHelper.setColors(xAxisColor, yAxisColor, zAxisColor);
-    scene.add(axesHelper);
 
     // AmbientLight
-    const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-    scene.add(ambientLight);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    group.add(ambientLight);
 
     // DirectionalLight
     const directionalLight = new THREE.DirectionalLight(0xffffff, 5,);
-    directionalLight.position.set(10, 10, 10);
+    directionalLight.position.set(20, 20, 20);
     directionalLight.target.position.set(-10, -10, -10);
-    scene.add(directionalLight);
-    scene.add(directionalLight.target);
+
+    group.add(directionalLight);
+    group.add(directionalLight.target);
 
 
     // sphere
     const geometry = new THREE.SphereGeometry( 1, 32, 16 );
     const material = new THREE.MeshLambertMaterial( { color: 0x222222 } );
     const sphere = new THREE.Mesh( geometry, material );
-    sphere.position.set(3, 3, 0);
-    scene.add( sphere );
-
-    // box
-    const boxGemotry = new THREE.BoxGeometry();
-    const boxMaterial = new THREE.MeshLambertMaterial({color: 0x0000ff});
-    const box = new THREE.Mesh(boxGemotry, boxMaterial);
-    box.position.set(-3, -3, 0);
-    scene.add(box);
+    sphere.receiveShadow = true;
+    sphere.castShadow = true;
+    sphere.position.set(3, 1, 0);
+    group.add( sphere );
 
     // postprocessing 渲染主场景
     const composer = new THREE.EffectComposer(renderer);
@@ -59,13 +45,10 @@ function main() {
     composer.addPass(renderPass);
 
 
-    // postprocessing 快速近似抗锯齿
-    const effectFXAA = new THREE.ShaderPass(THREE.FXAAShader);
-    effectFXAA.uniforms.resolution.value.set(1/window.innerWidth, 1/window.innerHeight);
-    composer.addPass(effectFXAA);
-
+    
     // postprocessing GPU 描边
     const outlinePass = new THREE.OutlinePass(new THREE.Vector2(window.innerWidth, window.innerHeight), scene, camera);
+    outlinePass.visibleEdgeColor = new THREE.Color(255, 0, 0);
     composer.addPass(outlinePass);
 
     // raycaster 
@@ -73,13 +56,13 @@ function main() {
     const mouse = new THREE.Vector2();
     let selectedObjects = [];
 
-    renderer.domElement.addEventListener( 'mousemove', onPointerMove );
+    renderer.domElement.addEventListener( 'pointermove', onPointerMove );
     function onPointerMove(event) {
         if (event.isPrimary === false) {
             return;
         }
         mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-        mouse.y = (event.clientY / window.innerHeight) * 2 + 1;
+        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
         
         checkIntersection();
     }
@@ -96,10 +79,11 @@ function main() {
 
         if (intersects.length > 0) {
             const selectedObject = intersects[0].object;
+            console.log(selectedObject.type);
             addSelectedObject(selectedObject);
             // 描边处理
             outlinePass.selectedObjects = selectedObjects;
-            console.log(outlinePass.selectedObjects);
+            // console.log(outlinePass.selectedObjects);
 
         }
     }
@@ -114,7 +98,7 @@ function main() {
     controls.maxDistance = 20;
 
     controls.maxPolarAngle = Math.PI / 2;
-    camera.position.set(0, 0, 10);
+    camera.position.set(5, 5, 5);
 
     function animate() {
         requestAnimationFrame(animate);
